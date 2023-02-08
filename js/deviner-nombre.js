9fe28d6bc93aa3dcdaa5b403e2b4276d355fa466
mahir.demirci <mahir.demirci@divcom.ch>
2023-02-08 10:28:12 +0100

    code java

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,41 @@
 /**
  * Jeu du nombre mystère
- * @author  Steve Fallet
+ * @author  Mahir Demirci
  * @version 1.0
- * @since   2022-08-30 (date de création)
+ * @since   2023-02-08 (date de création)
  */
 
-//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    'use strict';
+'use strict';
 
-   // Ton code de malade ici ...
+/**
+ * Fonction qui retourne un nombre aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 
-}()); //main IIFE
+function tireNombre(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+console.log(tireNombre(1, 100));
+let nbMystere = tireNombre(1, 100);
+alert(nbMystere);
+let nbEssais = 0;
+let reponse;
+
+do {
+    reponse = parseInt(prompt("Devinez un nombre entre 1 et 100 :"));
+    tries++;
+    if (reponse > nbMystere) {
+        alert("Trop grand ! Essayez à nouveau.");
+    } else if (reponse < nbMystere) {
+        alert("Trop petit ! Essayez à nouveau.");
+    }
+
+if (reponse === nbMystere) {
+    alert("Bravo, vous avez trouvé le nombre en " + tries + " essais !");
+} else {
+    alert("Désolé, vous avez épuisé tous vos essais. Le nombre était " + nbMystere + ".");
+}
+
+
